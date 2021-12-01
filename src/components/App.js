66fdf3a4de90e2a6098191461db96a35456e5277
AndreaAguado <andrea95am@gmail.com>
2021-12-01 13:24:12 +0100

    add html header, main and footer

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,22 @@
 import '../styles/App.scss';
 const App = () => {
-  return <div className="page">Hola mundo</div>;
+  return (
+    <div className="page">
+      <header>Netflix</header>
+      <main></main>
+      <footer className="footer">
+        <nav className="footer_nav">
+          <ul className="footer_links_list">
+            <li><a className="footer_link" href="https://twitter.com/andrea114am" target="_blank" rel="noreferrer" title="twitter.com/andrea114am"><i className="fab fa-twitter"></i></a></li>
+            <li><a className="footer_link" href="https://www.linkedin.com/in/andrea-aguado-moleon/" target="_blank" rel="noreferrer" title="linkedin.com/in/andrea-aguado-moleón/"><i className="fab fa-linkedin-in"></i></a></li>
+            <li><a className="footer_link" href="https://github.com/AndreaAguado" target="_blank" rel="noreferrer" title="github.com/AndreaAguado"><i className="fab fa-github"></i></a></li>
+          </ul>
+        </nav>
+        <small className="footer_small">Hecho con <span role="img" aria-label="emoji corazón" title="emoji corazón">&#10084;&#65039;</span> &copy; Andrea Aguado 2021</small>
+      </footer>
+      Hola mundo
+    </div>
+  );
 };
 
 export default App;
