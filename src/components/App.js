df66b802c07f7d32cabd3f82051d03dabe5f88c3
AndreaAguado <andrea95am@gmail.com>
2021-12-01 16:56:10 +0100

    add Footer component and import styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import '../styles/App.scss';
 import netflixLogo from '../images/Logo-Netflix.png';
+import Footer from './Footer';
 const App = () => {
 
   return (
@@ -10,7 +11,8 @@ const App = () => {
       <main>
         Buscar
       </main>
-      <footer className="footer">
+      <Footer></Footer>
+      {/* <footer className="footer">
         <nav className="footer_nav">
           <ul className="footer_links_list">
             <li><a className="footer_link" href="https://twitter.com/andrea114am" target="_blank" rel="noreferrer" title="twitter.com/andrea114am"><i className="fab fa-twitter"></i></a></li>
@@ -19,7 +21,7 @@ const App = () => {
           </ul>
         </nav>
         <small className="footer_small">Hecho con <span role="img" aria-label="emoji corazón" title="emoji corazón">&#10084;&#65039;</span> &copy; Andrea Aguado 2021</small>
-      </footer>
+      </footer> */}
     </div>
   );
 };
---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import '../styles/layout/Footer.scss';
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <nav className="footer_nav">
+                <ul className="footer_links_list">
+                    <li><a className="footer_link" href="https://twitter.com/andrea114am" target="_blank" rel="noreferrer" title="twitter.com/andrea114am"><i className="fab fa-twitter"></i></a></li>
+                    <li><a className="footer_link" href="https://www.linkedin.com/in/andrea-aguado-moleon/" target="_blank" rel="noreferrer" title="linkedin.com/in/andrea-aguado-moleón/"><i className="fab fa-linkedin-in"></i></a></li>
+                    <li><a className="footer_link" href="https://github.com/AndreaAguado" target="_blank" rel="noreferrer" title="github.com/AndreaAguado"><i className="fab fa-github"></i></a></li>
+                </ul>
+            </nav>
+            <small className="footer_small">Hecho con <span role="img" aria-label="emoji corazón" title="emoji corazón">&#10084;&#65039;</span> &copy; Andrea Aguado 2021</small>
+        </footer>
+    )
+}
+export default Footer;
