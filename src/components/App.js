f4aa870080394a29e277ff558c83a73ab1f55d16
AndreaAguado <andrea95am@gmail.com>
2021-12-02 20:35:23 +0100

    render results in list

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import { useEffect, useState } from 'react';
 import callToApi from '../services/callToApi';
 const App = () => {
   const [imagePath, setiImagePath] = useState('');
+  const [mediaToRender, setMediaToRender] = useState([]);
   useEffect(() => {
     callToApi.getConfiguration().then(response => {
       setiImagePath(response);
@@ -13,14 +14,32 @@ const App = () => {
   }, []);
   useEffect(() => {
     callToApi.getTrendingMedia().then(response => {
+      setMediaToRender(response);
       console.log(response);
     });
   }, []);
+
+  const renderMedia = () => {
+    return mediaToRender.map((movie, key) => {
+      return (
+        <li key={key} className="media_list_item">
+          <a href="·" title={movie.title} target="_blank" rel="noreferrer">
+            <img className="media_img" src={imagePath + movie.imagePath} alt={movie.title} />
+            <h3 className="media_title">{movie.title}</h3>
+          </a>
+        </li>
+      )
+    })
+  }
+
   return (
     <div className="page">
       <Header></Header>
-      <main>
+      <main className="main">
         Buscar
+        <ul className="media_list">
+          {renderMedia()}
+        </ul>
       </main>
       <Footer></Footer>
     </div>
