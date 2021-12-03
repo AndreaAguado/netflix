f2417e4325d0eb00caee9b88dbdd6a8f86356702
AndreaAguado <andrea95am@gmail.com>
2021-12-03 13:46:45 +0100

    create MediaDetails component and import styles

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,9 @@ import Footer from './Footer';
 import Header from './Header';
 import Main from './Main';
 import { useEffect, useState } from 'react';
-import { Switch, Route, useRouteMatch } from 'react-router-dom';
+import { Switch, Route, useRouteMatch, Link } from 'react-router-dom';
 import callToApi from '../services/callToApi';
+import MediaDetails from './MediaDetails';
 
 const App = () => {
   const [imagePath, setiImagePath] = useState('');
@@ -25,11 +26,11 @@ const App = () => {
   const renderMedia = () => {
     return mediaToRender.map((movie, key) => {
       return (
-        <li key={key} className="media_list_item">
-          <a className="media_list_item_content" href="·" title={movie.titleMovie ? movie.titleMovie : movie.titleShow} target="_blank" rel="noreferrer">
+        <li id={movie.id} key={movie.id} className="media_list_item">
+          <Link className="media_list_item_content" to={`/media/${movie.id}`} title={movie.titleMovie ? movie.titleMovie : movie.titleShow} target="_blank" rel="noreferrer">
             <img className="media_img" src={imagePath + movie.imagePath} alt={movie.titleMovie ? movie.titleMovie : movie.titleShow} />
             <h3 className="media_title">{movie.titleMovie ? movie.titleMovie : movie.titleShow}</h3>
-          </a>
+          </Link>
         </li>
       )
     })
@@ -48,7 +49,9 @@ const App = () => {
           <Footer></Footer>
         </Route>
         <Route path="/media/:id">
-          Peli
+          <Header></Header>
+          <MediaDetails media={clickedMedia}></MediaDetails>
+          <Footer></Footer>
         </Route>
       </Switch>
     </div>
---
+++ src/components/MediaDetails.js
@@ -0,0 +1,15 @@
+import '../styles/layout/MediaDetails.scss';
+const MediaDetails = (props) => {
+    return (
+        <section className="media_details_section">
+            <img className="media_details_img" src="https://image.tmdb.org/t/p/original/1P3QtW1IkivqDrKbbwuR0zCYIf8.jpg" alt="img" />
+            <div className="media_details_info">
+                <h3 className="media_details_title">Titulo</h3>
+                <p className="media_details_overview">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa laboriosam possimus molestiae asperiores, ut ipsum, odit laborum voluptatem voluptates sunt officiis quasi perferendis illo, facilis minima nesciunt. Accusantium, amet repudiandae?
+                </p>
+            </div>
+        </section>
+    )
+}
+export default MediaDetails;
