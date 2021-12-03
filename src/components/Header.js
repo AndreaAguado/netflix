9896474998c4b5d7bded992f009a165bcbeda9a0
AndreaAguado <andrea95am@gmail.com>
2021-12-03 17:30:26 +0100

    link header logo with homepage

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
 import '../styles/layout/Header.scss';
 import netflixLogo from '../images/Logo-Netflix.png';
+import { Link } from 'react-router-dom';
 const Header = () => {
     return (
         <header className="header">
-            <img className="header_logo" src={netflixLogo} alt="Logo Netflix" />
+            <Link to="/" className="header_link" title="Go to homepage">
+                <img className="header_logo" src={netflixLogo} alt="Logo Netflix" />
+            </Link>
         </header>
     )
 }
