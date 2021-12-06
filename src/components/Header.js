b78ed59404b522a3e171fad0f8f3bd105d363844
AndreaAguado <andrea95am@gmail.com>
2021-12-06 19:47:37 +0100

    change header styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,9 +17,19 @@ const Header = (props) => {
             <Link to="/" className="header_link" title="Go to homepage">
                 <img className="header_logo" src={netflixLogo} alt="Logo Netflix" />
             </Link>
-            <Link onClick={handleAllMedia} to="/">All</Link>
-            <Link onClick={handleMovies} to="/movies">Movies</Link>
-            <Link onClick={handleTVshows} to="/TVshows">TV Shows</Link>
+            <nav className="header_nav">
+                <ul className="header_nav_list">
+                    <li className="list_item">
+                        <Link className="header_nav_link" onClick={handleAllMedia} to="/">All</Link>
+                    </li>
+                    <li className="list_item">
+                        <Link className="header_nav_link" onClick={handleMovies} to="/movies">Movies</Link>
+                    </li>
+                    <li className="list_item">
+                        <Link className="header_nav_link" onClick={handleTVshows} to="/TVshows">TV Shows</Link>
+                    </li>
+                </ul>
+            </nav>
         </header>
     )
 }
