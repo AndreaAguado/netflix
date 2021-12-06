51df182f44a2454840ef808e3a71859892a83fd7
AndreaAguado <andrea95am@gmail.com>
2021-12-06 13:09:16 +0100

    use placeholder if API doesn't provide image

--- src/components/MediaDetails.js
+++ src/components/MediaDetails.js
@@ -18,7 +18,7 @@ const MediaDetails = (props) => {
                     <p className="return_link_title">Return</p>
                 </Link>
             </div>
-            < img className="media_details_img" src={props.imagePath + props.media.banner} alt="img" />
+            < img className="media_details_img" src={props.media.banner ? props.imagePath + props.media.banner : "https://via.placeholder.com/732x411/ff3b3b/000000/?text=TV"} alt="img" />
             <div className="media_details_info">
                 <h3 className="media_details_title">{props.media.titleMovie ? props.media.titleMovie : props.media.titleShow}</h3>
                 <p className="media_details_overview">
